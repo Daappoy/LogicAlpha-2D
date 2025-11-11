2f04389c854520421e92ba7602c4b40ddf91c39f
Daappoy <m.alghifaridaffa@gmail.com>
2025-11-12 01:03:26 +0700

    added script for the Pausing mechanic

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
+    
+    private void PauseGame()
+    {
+        if (GameIsPaused)
+        {
+            pauseMenuUI.SetActive(false);
+            Time.timeScale = 1f;
+            GameIsPaused = false;
+        }
+        else
+        {
+            pauseMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            GameIsPaused = true;
+        }
+    }
+}
